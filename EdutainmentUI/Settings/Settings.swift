db44ec5f6b3da8928be9331cd0d76b8b2caba524
Damie <damie@Damie-MacBook-Pro-2.local>
2022-09-30 22:36:10 +0100

    initial commit

---
+++ EdutainmentUI/Settings/Settings.swift
@@ -0,0 +1,9 @@
+//
+//  Settings.swift
+//  EdutainmentUI
+//
+//  Created by Damie on 30.09.2022.
+//
+
+import Foundation
+
---
+++ EdutainmentUI/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsViewController.swift
+//  EdutainmentUI
+//
+//  Created by Damie on 30.09.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    enum OperationType: String {
+        case addition = "⊕", subtraction = "⊖", multiplication = "⨷", division = "⨸", any = "☸"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func TaskTypeBtn(_ sender: UIButton) {
+        
+    }
+    
+}
