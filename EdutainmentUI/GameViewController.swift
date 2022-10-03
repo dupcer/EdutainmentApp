a7a50857582fbb4b641f899ad989e6bd1eea8548
Damie <damie@Damie-MacBook-Pro-2.local>
2022-10-03 18:19:00 +0100

    trying to get rid of the detail view overriding by a new window being ppushed

--- EdutainmentUI/GameViewController.swift
+++ EdutainmentUI/GameViewController.swift
@@ -8,50 +8,85 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
-        if flow != nil {
+        //        if flow != nil {
+        //            do {
+        //                try task = flow?.getNewTask()
+        //                refreshUI(task: task!)
+        //            } catch let error as Flow.GameError {
+        //                self.errorMessageController.gameErrorMsg(for: error)
+        //            } catch {
+        //                NSLog("")
+        //            }
+        //
+        //        }
+    }
+    
+    let errorMessageController = ErrorMessagesController()
+    var flow: Flow? {
+        didSet {
             do {
                 try task = flow?.getNewTask()
-                setValuesToLabels(task: task!)
             } catch let error as Flow.GameError {
                 self.errorMessageController.gameErrorMsg(for: error)
             } catch {
-                NSLog("")
+                NSLog("Error was cought, but not properly handeled")
             }
+            refreshUI()
+        }
+    }
+    var task: Task? {
+        didSet {
             
         }
     }
     
-    let errorMessageController = ErrorMessagesController()
-    var flow: Flow?
-    var task: Task?
-
+    @IBOutlet weak var startNewGameLabel: UILabel! {
+        didSet {
+            if flow != nil {
+                startNewGameLabel.isHidden = false
+            } else {
+                startNewGameLabel.isHidden = true
+            }
+        }
+    }
     @IBOutlet weak var varOneLabel: UILabel!
     @IBOutlet weak var operationSignLabel: UILabel!
     @IBOutlet weak var varTwoLabel: UILabel!
-    
     
     @IBOutlet weak var option1Label: UIButton!
     @IBOutlet weak var option2Label: UIButton!
     @IBOutlet weak var option3Label: UIButton!
     
-    private func setValuesToLabels(task: Task) {
-        varOneLabel.text = String(task.varOne)
-        varTwoLabel.text = String(task.varTwo)
+    private func refreshUI() {
+        loadViewIfNeeded()
         
-        switch task.operation {
-        case .addition: operationSignLabel.text = "+"
-        case .subtraction: operationSignLabel.text = "-"
-        case .division: operationSignLabel.text = "÷"
-        case .multiplication: operationSignLabel.text = "×"
-        default: operationSignLabel.text = "+"
+        if flow == nil {
+            startNewGameLabel.isHidden = false
+        } else {
+            startNewGameLabel.isHidden = true
+            
+            varOneLabel.text = String(task?.varOne ?? 0)
+            varTwoLabel.text = String(task?.varTwo ?? 0)
+            
+            switch task?.operation {
+            case .addition: operationSignLabel.text = "+"
+            case .subtraction: operationSignLabel.text = "-"
+            case .division: operationSignLabel.text = "÷"
+            case .multiplication: operationSignLabel.text = "×"
+            default: operationSignLabel.text = "+"
+            }
+            
+            // TODO: update buttons
         }
+        
+        
         
     }
     
